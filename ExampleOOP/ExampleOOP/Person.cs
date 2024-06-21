e378b786841230bfc8e705193e2e432eff9f3897
Tina Caron <tcaron@nait.ca>
2024-06-20 21:47:29 -0600

    Added Report and Finished form.

--- ExampleOOP/ExampleOOP/Person.cs
+++ ExampleOOP/ExampleOOP/Person.cs
@@ -56,6 +56,10 @@ namespace ExampleOOP
 
         public Person(string firstName, string lastName, ResidentAddress address, List<Employment> employmentPositions)
         {
+            if(address.Number <= 0)
+            {
+                throw new ArgumentException("Street Address cannot be 0 or negative.");
+            }
             FirstName = firstName;
             LastName = lastName;
             ResidentAddress = address;
@@ -80,6 +84,19 @@ namespace ExampleOOP
                 throw new ArgumentNullException("No employment data provided");
             }
             EmploymentPositions.Add(employment);
+        }
+
+        public override string ToString() => $"{FirstName},{LastName},{ResidentAddress.Number},{ResidentAddress.Street},{ResidentAddress.City},{ResidentAddress.Province},{ResidentAddress.PostalCode}";
+
+        public static Person Parse(string item)
+        {
+            string[] parts = item.Split(',');
+
+            if (parts.Length != 7)
+            {
+                throw new FormatException($"String not in the expected format. Missing or excessive value(s): {item}");
+            }
+            return new Person(parts[0], parts[1], new ResidentAddress(int.Parse(parts[2]), parts[3], parts[4], parts[5], parts[6]), null);
         }
         #endregion
     }
