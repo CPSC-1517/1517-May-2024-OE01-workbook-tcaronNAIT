dbd5227114a8ee6070642fec3031a2007c4aa6e0
Tina Caron <tcaron@nait.ca>
2024-07-16 19:38:56 -0600

    Created Blazor Project and Solution

---
+++ ExampleWestWindSolution/ExampleWestWind/Program.cs
@@ -0,0 +1,26 @@
+using ExampleWestWind.Components;
+using MudBlazor.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+	.AddInteractiveServerComponents();
+
+builder.Services.AddMudServices();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+	app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+	.AddInteractiveServerRenderMode();
+
+app.Run();
